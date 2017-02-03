5e90ecad04f3ce57c1c92ae32d49c4f4096931ee
John W. O'Neill <oneill.john.w@gmail.com>
2017-02-03 02:20:34 -0500

    all kinds of stuff

--- statusbar.sh
+++ statusbar.sh
@@ -10,11 +10,12 @@ FONT="-*-terminus-medium-*-*-*-14-*-*-*-*-*-*-*"
 FGCOLOR="#666666"
 BGCOLOR="#000000"
 WIDTH=$(($CURRENT_XRES/2))
+WIDTH=960
 HEIGHT=16
 XPOS=$(($CURRENT_XRES - $WIDTH))
 YPOS=0
 if [[ -e ~/.display2 ]]; then
-	YPOS=780
+  YPOS=780
 fi
 DELIM="^fg(#333333) | ^fg()"
 NOWPLAYING_TEXT_SIZE=60
@@ -29,8 +30,8 @@ MPD_HOST='localhost'
 while true; do
 
   ### Show battery if not connected to AC
-  AC_STATE=$(cat /proc/acpi/ac_adapter/AC/state | sed -e 's/state:[ \t]*//' -e 's/ *$//')
-  if [ $AC_STATE != "on-line" ]
+  AC_STATE=$(cat /sys/class/power_supply/AC/online)
+  if [ $AC_STATE != "1" ]
   then
     # CHARGE=$(acpi -b | awk -F, '{print $2}')
     TIME_REMAINING=$(acpi -b | awk '{print $5}')
@@ -40,34 +41,33 @@ while true; do
     BATTERY=""
   fi
 
-	### Date and Time
-	DATE=$(date +"%a, %b %d${DELIM}%H:%M^fn("-*-terminus-medium-*-*-*-12-*-*-*-*-*-*-*"):%S^fn()")
-	DATE="^ca(1,xclock)$DATE^ca()"
+  ### Date and Time
+  DATE=$(date +"%a, %b %d${DELIM}%H:%M^fn("-*-terminus-medium-*-*-*-12-*-*-*-*-*-*-*"):%S^fn()")
+  DATE="^ca(1,xclock)$DATE^ca()"
 
-	## CPU Temp
-	CPUTEMP=`acpi -ft | grep "Thermal 0" |sed -e 's/Thermal [01]:.*\,//' -e 's/degrees.*//'`
+  ## CPU Temp
+  CPUTEMP=`acpi -ft | grep "Thermal 0" |sed -e 's/Thermal [01]:.*\,//' -e 's/degrees.*//'`
 
-	CPUTEMP_INT=`echo "($CPUTEMP+0.5)/1" | bc`
+  # round
+  CPUTEMP_INT=`echo "($CPUTEMP+0.5)/1" | bc`
 
-	if [ $CPUTEMP_INT -gt 200 ]
-	then
-		CPUTEMPCOLOR='#DF0101'
+  if [ $CPUTEMP_INT -gt 200 ]
+  then
+    CPUTEMPCOLOR='#DF0101'
+  elif [ $CPUTEMP_INT -gt 165  ]
+  then
+    CPUTEMPCOLOR='#8A2908'
+  else
+    CPUTEMPCOLOR=$FGCOLOR
+  fi
+  CPUTEMP="^fg(${CPUTEMPCOLOR})${CPUTEMP}^bg()"
 
-	elif [ $CPUTEMP_INT -gt 165  ]
-	then
-		CPUTEMPCOLOR='#8A2908'
-	else
-		CPUTEMPCOLOR=$FGCOLOR
-	fi
+  ##Weather
+  [ $WEATHER_TIMER == -1 ] && {
+  WEATHER=`weather-report | grep "Temperature\|Weather\|Sky conditions" | sed -e 's/(.*)//' -e 's/Weather://' -e 's/Temperature://' -e 's/\..*//g' -e 's/Sky conditions://' -e 's/\ \ //g' | tr '\n' " "`
 
-	CPUTEMP="^fg(${CPUTEMPCOLOR})${CPUTEMP}^bg()"
-
-	##Weather
-	[ $WEATHER_TIMER == -1 ] && {
-	WEATHER=`weather | grep "Temperature\|Weather\|Sky conditions" | sed -e 's/(.*)//' -e 's/Weather://' -e 's/Temperature://' -e 's/\..*//g' -e 's/Sky conditions://' -e 's/\ \ //g' | tr '\n' " "`
-
-	#Check every 20 mins
-	WEATHER_TIMER=1000
+  #Check every 20 mins
+  WEATHER_TIMER=1000
 }
 
 ###Volume
@@ -105,54 +105,55 @@ while true; do
 
 if [ "$(pidof mpd)" ]; then
 
-	NOWPLAYING=$(mpc  -h $MPD_HOST|sed -n 2p)
-	if [ $(echo "$NOWPLAYING" | grep "paused") ]; then
-		NOWPLAYING="--PAUSED--"
-	else
-	NOWPLAYING=$(mpc -h $MPD_HOST -f "%file%~%name%~%artist%~%title%"| head -n 1)
-	IFS='~'
-	set $NOWPLAYING
-	if [ $(echo $NOWPLAYING|grep "^http") ]
-	then
-		if [ $4 ]
-		then
-			NOWPLAYING="$4"
-		else
-			NOWPLAYING="$2"
-		fi
-	else
-		NOWPLAYING="$3 - $4"
-	fi
+  NOWPLAYING=$(mpc  -h $MPD_HOST|sed -n 2p)
+  if [ $(echo "$NOWPLAYING" | grep "paused") ]; then
+    NOWPLAYING="--PAUSED--"
+  else
+    NOWPLAYING=$(mpc -h $MPD_HOST -f "%file%~%name%~%artist%~%title%"| head -n 1)
+    IFS='~'
+    set $NOWPLAYING
+    if [ $(echo $NOWPLAYING|grep "^http") ]
+    then
+      if [ $4 ]
+      then
+        NOWPLAYING="$4"
+      else
+        NOWPLAYING="$2"
+      fi
+    else
+      NOWPLAYING="$3 - $4"
+    fi
 
-	if [[ ${#NOWPLAYING} -gt $NOWPLAYING_TEXT_SIZE ]]; then
-		# Text is too long, need to scroll
-		# from https://github.com/livibetter/dotfiles/blob/master/dzen/status.sh
-		#http://blog.yjl.im/2010/12/my-dzen-status-bar.html
-		if [[ $mpd_text_dir ]]; then
-			# scroll right
-			if ((++mpd_text_pos >= ${#NOWPLAYING} + 5 - NOWPLAYING_TEXT_SIZE)); then
-				mpd_text_pos=$((${#NOWPLAYING} - NOWPLAYING_TEXT_SIZE))
-				mpd_text_dir=
-			fi
-		else
-			# scroll left, will be first direction since $mpd_text_dir is unset by default
-			if ((--mpd_text_pos <= -5)); then
-				mpd_text_pos=0
-				mpd_text_dir=1
-			fi
-		fi
-		pos=$mpd_text_pos
-		[[ $pos -lt 0 ]] && pos=0
-		((pos > ${#NOWPLAYING} - NOWPLAYING_TEXT_SIZE)) && pos=$((${#NOWPLAYING} - NOWPLAYING_TEXT_SIZE))
-	fi
-NOWPLAYING="^fn("-*-terminus-medium-*-*-*-12-*-*-*-*-*-*-*")(mpd) ${NOWPLAYING:$pos:$NOWPLAYING_TEXT_SIZE}^fn()"
+    if [[ ${#NOWPLAYING} -gt $NOWPLAYING_TEXT_SIZE ]]; then
+      # Text is too long, need to scroll
+      # from https://github.com/livibetter/dotfiles/blob/master/dzen/status.sh
+      #http://blog.yjl.im/2010/12/my-dzen-status-bar.html
+      if [[ $mpd_text_dir ]]; then
+        # scroll right
+        if ((++mpd_text_pos >= ${#NOWPLAYING} + 5 - NOWPLAYING_TEXT_SIZE)); then
+          mpd_text_pos=$((${#NOWPLAYING} - NOWPLAYING_TEXT_SIZE))
+          mpd_text_dir=
+        fi
+      else
+        # scroll left, will be first direction since $mpd_text_dir is unset by default
+        if ((--mpd_text_pos <= -5)); then
+          mpd_text_pos=0
+          mpd_text_dir=1
+        fi
+      fi
+      pos=$mpd_text_pos
+      [[ $pos -lt 0 ]] && pos=0
+      ((pos > ${#NOWPLAYING} - NOWPLAYING_TEXT_SIZE)) && pos=$((${#NOWPLAYING} - NOWPLAYING_TEXT_SIZE))
+    fi
+    NOWPLAYING="^fn("-*-terminus-medium-*-*-*-12-*-*-*-*-*-*-*")(mpd) ${NOWPLAYING:$pos:$NOWPLAYING_TEXT_SIZE}^fn()"
+  fi
+else
+  NOWPLAYING=""
 fi
-	else
-		NOWPLAYING=""
+
+if [ "$(pidof pianobar)" ]; then
+  NOWPLAYING=$(cat ~/.config/pianobar/nowplaying)
 fi
-
-
-###### Logout button
 
 ### Put the string togather
 #echo ${NOWPLAYING}${DELIM}${CPUTEMP}${DELIM}${UNREAD}${DELIM}${WEATHER}${DELIM}${DATE}${LOGOUT}
